72be8d49ff098c1a310c08fac1ec9ccbe7d5e20c
Fabian Sigfridsson <69erik355@gmail.com>
2024-03-12 11:26:09 +0100

    korekt sql fråga

--- routes/index.js
+++ routes/index.js
@@ -19,20 +19,37 @@ router.get('/login', async function (req, res) {
 })
 
 router.post('/login', 
-query(username).isLength({min: 4, max: 30}), 
-query(password).isLength({min: 4, max: 30}),  
+query('username').isLength({min: 4, max: 30}), 
+query('password').isLength({min: 4, max: 30}),  
 async function(req, res){
     const result = validationResult
     
     try{
-        const user = await pool.promise().query(`SELECT * FROM alea_lacta_est_user`)
+        const [user] = await pool.promise().query(`SELECT * FROM alea_lacta_est_user WHERE alea_lacta_est_user.\`name\` = '${req.body.username}'`)
+        console.log(user)
+        console.log(user[0])
+        console.log(user[0].password)
+        console.log(encryptPassword('test'))
+        console.log(encryptPassword(req.body.password))
 
-     } catch{
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            let encryptPassword = hash
+        })
+        
+        //if (user[0].password === encryptPassword(req.body.password)){
+        //    req.session.name = req.body.username
+        //    res.redirect(`/user/:${req.body.username}`, {name: req.body.username})
+        //} else {
+            res.json({user, encrypted:encryptPassword(req.body.password)})
+        //}
+
+     } catch (error){
+        console.log(error)
         res.redirect('/')
     }
 
-    req.session.name = req.body.username
-    res.redirect(`user/:${req.body.name}`)
+    //req.session.name = req.body.username
+    //res.redirect(`user/:${req.body.name}`)
 })
 router.get('/user/:name', async function (req, res) {
     console.log(req.session.name)
@@ -82,7 +99,6 @@ router.get('/hashtest', async function (req, res){
     })
 
 })
-
 
 
 module.exports = router
